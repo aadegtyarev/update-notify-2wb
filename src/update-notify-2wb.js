78ef0f3b69c155323bef18f5e3381cd55f0a093a
wb-adegtyarev <a.degtyarev@wirenboard.com>
2021-10-02 01:11:21 +0400

    v.1.1.0

--- src/update-notify-2wb.js
+++ src/update-notify-2wb.js
@@ -1,99 +1,130 @@
-/*v. 1.0.0*/
-var deviceName = "system";
-var packagesControl = "New ";
+/*v. 1.1.0*/
+var deviceName = "update-sensor";
+var logControlName = "Log";
+var btnControlName = "Check for updates";
+var updateIsRunning = false;
 
-checkUpdate();
 
-defineRule("cron_check_update", {
-    when: cron("0 0 5,11 * *"),
-    then: function () {
-        checkUpdate();
-    }
-})
+init();
 
 function checkUpdate() {
-    clearStatus();
+  if (!updateIsRunning){
+    updateIsRunning = true;
+    writeStatus("Search for updates…");
     runShellCommand("apt update", {
-        captureOutput: true,
-        exitCallback: function (exitCode, capturedOutput) {
-            if (exitCode === 0) {
-                packagesCount = getPackagesCount(capturedOutput);
-                thereAreUpdates = packagesCount !== 0;
-                if (thereAreUpdates) {
-                    writeUpdatePackageList();
-                } else {
-                    clearStatus();
-                }
-                return;
-            }
-        }
+      captureOutput: true,
+      exitCallback: function (exitCode, capturedOutput) {
+        if (exitCode === 0) {
+          packagesCount = getPackagesCount(capturedOutput);
+          thereAreUpdates = packagesCount !== 0;
+          if (thereAreUpdates) {
+            writeUpdatePackageList();
+          } else {
+            writeStatus("No updates");
+          }
+          
+          updateIsRunning = false;
+          return;
+        }        
+      }
     });
+  }
 }
 
-function clearStatus() {
-    removeControl(packagesControl);
+function init() {
+  createVirtualDevice();
+
+  defineRule("cron_check_update", {
+    when: cron("0 0 5,11 * *"),
+    then: function () {
+      checkUpdate();
+    }
+  });
+
+  defineRule("click_update_button", {
+    whenChanged: "{}/{}".format(deviceName, btnControlName),
+    then: function (newValue, devName, cellName) {
+      checkUpdate();
+    }
+  });
+
+  checkUpdate();
+}
+
+function createVirtualDevice() {
+  device = defineVirtualDevice(deviceName, {
+    cells: {}
+  });
+
+  device.addControl(logControlName, { type: "text", value: "", readonly: true });
+  device.addControl(btnControlName, { type: "pushbutton", value: false, readonly: false });
+
+}
+
+function writeStatus(newStatus) {
+  writePackagesList(newStatus);
+}
+
+function setButtonDisable(status) {
+  dev["{}/{}#readonly".format(deviceName, btnControlName)] = status;
 }
 
 function writeUpdatePackageList() {
-    writePackagesList("");
-    runShellCommand("apt list --upgradable", {
-        captureOutput: true,
-        exitCallback: function (exitCode, capturedOutput) {
-            if (exitCode === 0) {
-                strings = getArrayString(capturedOutput);
-                packagesList = getPackages(strings);
+  writePackagesList("");
+  runShellCommand("apt list --upgradable", {
+    captureOutput: true,
+    exitCallback: function (exitCode, capturedOutput) {
+      if (exitCode === 0) {
+        strings = getArrayString(capturedOutput);
+        packagesList = getPackages(strings);
 
-                writePackagesList(packagesList);
-                return;
-            }
-        }
-    });
+        writePackagesList(packagesList);
+        return;
+      }
+    }
+  });
 }
 
 function writePackagesList(list) {
-    device = getDevice(deviceName);
-    ctrlID = packagesControl;
+  dev[deviceName][logControlName] = list;
+}
 
-    if (!device.isControlExists(ctrlID)) {
-        newControl = { type: "text", value: "", readonly: true };
-        device.addControl(ctrlID, newControl);
-    }
-
-    dev["{}/{}".format(device.getId(), ctrlID)] = list;
+function getTimeStamp() {
+  return new Date();
 }
 
 function getPackages(strings) {
-    packagesList = "";
-    strings.forEach(function (item, _index, _array) {
-        indexSlash = item.indexOf("/");
-        if (indexSlash > 0) {
-            packagesList += "{}, ".format(item.slice(0, indexSlash));
-        }
-    });
+  packagesList = "";
+  strings.forEach(function (item, _index, _array) {
+    indexSlash = item.indexOf("/");
+    if (indexSlash > 0) {
+      packagesList += "{}, ".format(item.slice(0, indexSlash));
+    }
+  });
 
-    packagesList = packagesList.trim();
-    return packagesList.slice(0, packagesList.length - 1);
+  packagesList = packagesList.trim();
+  return packagesList.slice(0, packagesList.length - 1);
 }
 
 function getPackagesCount(commandOut) {
-    lastString = getLastString(commandOut);
-    return getNumber(lastString);
+  lastString = getLastString(commandOut);
+  return getNumber(lastString);
 }
 
 function getLastString(text) {
-    strings = getArrayString(text);
-    return strings[strings.length - 2];
+  strings = getArrayString(text);
+  return strings[strings.length - 2];
 }
 
 function getArrayString(text) {
-    return text.split("\n");
+  return text.split("\n");
 }
 
 function getNumber(text) {
-    return Number(text.replace(/[^+\d]/g, ''));
+  return Number(text.replace(/[^+\d]/g, ''));
 }
 
 function removeControl(name) {
-    device = getDevice(deviceName);
-    device.removeControl(name);
+  device = getDevice(deviceName);
+  device.removeControl(name);
 }
